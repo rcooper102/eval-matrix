53a9bd4812dd42eb2fcb9be0c68e43e60a6f4ff5
Ryan Cooper <ryan.cooper@groupbyinc.com>
2018-09-19 14:38:15 -0400

    added question ids

--- src/config/attn-to-detail.js
+++ src/config/attn-to-detail.js
@@ -6,35 +6,41 @@ export const ATTN_TO_DETAIL = [
     title: `Syntax standards (no <br> tags, no deprecated tags, no use of attributes when CSS should be used, etc)`,
     description: 'tbd',
     weight: 1, 
+    id: 'syntax-standards',
   },
   {
     type: QUESTION_TYPES.RANGE,
     title: `Syntax consistency (naming conventions, use of SCSS variables, etc)`,
     description: 'tbd',
     weight: 1, 
+    id: 'syntax-consistency',
   },
   {
     type: QUESTION_TYPES.RANGE,
     title: `Follows best practices (locale, unit tests, etc)`,
     description: 'tbd',
     weight: 1, 
+    id: 'best-practices',
   },
   {
     type: QUESTION_TYPES.RANGE,
-    title: `THOROUGH testing (with screenshots)`,
+    title: `THOROUGH testing`,
     description: 'tbd',
     weight: 1, 
+    id: 'thorough-testing',
   },
   {
     type: QUESTION_TYPES.RANGE,
     title: `Tests outlier use cases and potential ripple effects of changes made to the code base.`,
     description: 'tbd',
     weight: 1, 
+    id: 'ripple-effects'
   },
   {
     type: QUESTION_TYPES.RANGE,
     title: `Avoids "work arounds" and "hacky" solutions in order to lazily solve problems in a way that introduces future risk. `,
     description: 'tbd',
     weight: 1, 
+    id: 'avoids-hacks',
   },
 ];
