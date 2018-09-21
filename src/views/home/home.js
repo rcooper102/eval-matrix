d1e99148cae1946a14fde222e25d36018c1d9522
Ryan Cooper <ryan.cooper@groupbyinc.com>
2018-09-21 09:21:27 -0400

    cleaned up home

--- src/views/home/home.js
+++ src/views/home/home.js
@@ -4,15 +4,15 @@ import { Reminder } from '../../components/reminder';
 
 export class Home extends React.Component {
   render() {
-    return	<div className='Home View'>
-    		<h1>Welcome to the</h1>
-        <h2>GBI Front End Developer Evaluation Tool</h2>
-        <p>The GBI evaluation tool is mechanism used to evaluate the progress of individual team member's in their career.</p>
-        <Reminder />
-        <div className='navigation'>
-    			<a href='#name'>Begin</a>
-    		</div>
-    	</div>
-    	;
+    return  
+    <div className='Home View'>
+      <h1>Welcome to the</h1>
+      <h2>GBI Front End Developer Evaluation Tool</h2>
+      <p>The GBI evaluation tool is mechanism used to evaluate the progress of individual team member's in their career.</p>
+      <Reminder />
+      <div className='navigation'>
+      <a href='#name'>Begin</a>
+      </div>
+    </div>
   }
 }
