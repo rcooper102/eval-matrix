6386f0214aa7f20b7fadb80cc3b328385479ebfa
Ryan Cooper <ryan.cooper@groupbyinc.com>
2018-09-21 14:46:06 -0400

    updates.

--- src/config/attn-to-detail.js
+++ src/config/attn-to-detail.js
@@ -3,43 +3,36 @@ import { QUESTION_TYPES } from "../constants";
 export const ATTN_TO_DETAIL = [
   {
     type: QUESTION_TYPES.RANGE,
-    title: `Syntax standards (no <br> tags, no deprecated tags, no use of attributes when CSS should be used, etc)`,
-    description: 'tbd',
+    title: `Syntax Standards`,
+    description: 'Adheres to agreed upon syntax standards consistently. (no <br> tags, no deprecated tags, no use of attributes when CSS should be used, etc)',
     weight: 1, 
     id: 'syntax-standards',
   },
   {
-    type: QUESTION_TYPES.RANGE,
-    title: `Syntax consistency (naming conventions, use of SCSS variables, etc)`,
-    description: 'tbd',
-    weight: 1, 
-    id: 'syntax-consistency',
-  },
-  {
     type: QUESTION_TYPES.AGREE,
-    title: `Follows best practices (locale, unit tests, etc)`,
-    description: 'tbd',
+    title: `Follows Best Practices`,
+    description: [
+      'Makes an effort to adhere to best practices as much as possible.',
+    ],
     weight: 1, 
     id: 'best-practices',
   },
   {
     type: QUESTION_TYPES.RANGE,
-    title: `THOROUGH testing`,
-    description: 'tbd',
+    title: `Tests Work`,
+    description: [
+      'Thoroughly tests all work before it goes out.',
+      'Always looks to test alternative or unexpected use cases.',
+      'Tests in a variety of browsers to ensure stability.',
+      'Predicts potential ripple effects of work and tests accordingly.',
+    ],
     weight: 1, 
     id: 'thorough-testing',
   },
   {
     type: QUESTION_TYPES.AGREE,
-    title: `Tests outlier use cases and potential ripple effects of changes made to the code base.`,
-    description: 'tbd',
-    weight: 1, 
-    id: 'ripple-effects'
-  },
-  {
-    type: QUESTION_TYPES.AGREE,
-    title: `Avoids "work arounds" and "hacky" solutions in order to lazily solve problems in a way that introduces future risk. `,
-    description: 'tbd',
+    title: `Hacky Solutions`,
+    description: 'Avoids "work arounds" and "hacky" solutions in order to lazily solve problems in a way that introduces future risk.',
     weight: 1, 
     id: 'avoids-hacks',
   },
