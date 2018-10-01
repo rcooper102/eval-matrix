0437268ad5f2033e5158ad0b97e5216a771ae54f
Ryan Cooper <ryan.cooper@groupbyinc.com>
2018-10-01 15:41:15 -0400

    added weight to summary

--- src/views/question-summary/question-summary.js
+++ src/views/question-summary/question-summary.js
@@ -21,7 +21,7 @@ export class QuestionSummary extends React.Component {
   createQuestions(questions, index, category) {
     return questions.map((item, i) => {
       return <div key={i}>
-        <h3>{index}-{ i+1 }: { item.title } <span>({category}:{item.id})</span></h3>
+        <h3>{index}-{ i+1 }: { item.title } <span>({category}:{item.id}, weight: { item.weight })</span></h3>
         <p>{ Array.isArray(item.description) ? item.description.map((p, j) => <span key={j}>{p}</span>) : <span>{item.description}</span> } </p>
         <p className='options'><strong>Possible Responses:</strong> { QUESTION_RANGES[item.type].join(', ') }.</p>
       </div>
@@ -30,11 +30,16 @@ export class QuestionSummary extends React.Component {
 
   render() {
     return  <div className='QuestionSummary View'>
+      <h1>Question Summary</h1>
+      <span>
       <p>Question Count: { Object.keys(Controller.data.answers).length }</p>
+      <p>Each question has a weight which determines their individual impact in determining respective category score.</p>
+      <p>Each category has a weight which determines category impact when determining total score.</p>
+      </span>
       {
         Object.keys(Controller.config.categories).map((item, i) => {
           return <div key={i}>
-          <h2>{Controller.config.categories[item].label} ({Controller.config.categories[item].questions.length})</h2>
+          <h2>{Controller.config.categories[item].label} <span>({Controller.config.categories[item].questions.length} questions, weight: { Controller.config.categories[item].weight })</span></h2>
           { this.createQuestions(Controller.config.categories[item].questions, i+1, item) }
           </div>
         })
