4a3b7c13f2259fdceaf9b2473dc26ca54af45828
Ryan Cooper <ryan.cooper@groupbyinc.com>
2018-10-03 11:25:40 -0400

    Added progress bars to role progress

--- src/components/score-role-progress.js
+++ src/components/score-role-progress.js
@@ -22,7 +22,7 @@ export class ScoreRoleProgress extends React.Component {
     const score = Controller.scoreSummary;
     return  <div className='ScoreRoleProgress'>
         <h4 className={this.state.open ? 'open' : 'closed'} onClick={this.onOpen}>{ data.label }</h4>
-        <div style={{ display: this.state.open ? 'block' : 'none' }}>
+        <div className="report" style={{ display: this.state.open ? 'block' : 'none' }}>
         <ol className='description'>{
           data.description ? data.description.map((item, i) => { 
             return <li key={i}>{item}</li> 
@@ -36,7 +36,7 @@ export class ScoreRoleProgress extends React.Component {
               let total = null;
               if(data.requiredCategoryScore[item].total) {
                 const max = Controller.config.categories[item].weight *  Controller.config.constants.MAX_SCORE * data.requiredCategoryScore[item].total;
-                total = <span className={ score.categories[item] >= max ? 'complete' : '' }>{ score.categories[item] ? score.categories[item] : 0 } / { max } ( { score.categories[item] ? Math.round(score.categories[item]/max*100) : 0 }% )</span>
+                total = <span className={ score.categories[item] >= max ? 'complete' : '' }><div className="bar" style={{ width: `${score.categories[item]/max < 1 ? score.categories[item]/max*100 : 100}%` }}></div><div style={{ display: score.categories[item]/max >= 1 ? 'none' : 'block' }} className="label">{ Math.round(score.categories[item]/max*100) }%</div></span>
               }
               return <li key={i}>
                 <h5>{Controller.config.categories[item].label} {total}</h5>
