766ed7403bbaf7dd6fe70d759e85553606111c38
Ryan Cooper <ryan.cooper@groupbyinc.com>
2018-10-04 16:03:24 -0400

    Fixed typo in width

--- src/components/score-question.js
+++ src/components/score-question.js
@@ -13,7 +13,7 @@ export class ScoreQuestion extends React.Component {
 
     let result = null;
     if(score) {
-      result = <h6><strong>{ score !== -1 ? question[Math.round(score * (question.length - 1))] : 'No Answer' }</strong><span className='progress'><div className="bar" style={{ width: `${score*100}px`}}></div><div className='label'>{ score !== -1 ? `${Math.round(score*100)}%` : 'n/a' }</div></span></h6>
+      result = <h6><strong>{ score !== -1 ? question[Math.round(score * (question.length - 1))] : 'No Answer' }</strong><span className='progress'><div className="bar" style={{ width: `${score*100}%`}}></div><div className='label'>{ score !== -1 ? `${Math.round(score*100)}%` : 'n/a' }</div></span></h6>
     }
 
     const comment = Controller.getQuestionComment(Controller.createId(this.props.category, this.props.question.id));
